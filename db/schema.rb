68cfda914a81f7cadaec374005d924f7589753e2
MariaRomero <fantasysystems@gmail.com>
2016-06-08 17:58:51 +0100

    added username to sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608135102) do
+ActiveRecord::Schema.define(version: 20160608164428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20160608135102) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "description"
-    t.json     "pictures"
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
@@ -52,9 +51,11 @@ ActiveRecord::Schema.define(version: 20160608135102) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
 
 end
