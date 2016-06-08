d78aa378b8e914468fd51b9c89351c443c45186f
MariaRomero <fantasysystems@gmail.com>
2016-06-08 11:35:38 +0100

    editing stalls added

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160607151534) do
+ActiveRecord::Schema.define(version: 20160607153805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
